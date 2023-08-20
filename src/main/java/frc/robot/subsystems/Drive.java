d12a3a86bda48b1585559957b993d38cab454df1
Logan MacAskill <logan@macaskill.com>
2023-08-19 18:01:25 -0700

    Add CAN ID constants and declare sparks

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
@@ -22,6 +21,7 @@ import java.util.function.Supplier;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import frc.robot.commands.*;
 import frc.robot.Constants;
@@ -30,33 +30,33 @@ import com.ctre.phoenix.sensors.CANCoder;
 
 public class Drive extends SubsystemBase {
 
-  private CANSparkMax[] driveMotors;
-  private CANSparkMax[] steerMotors;
+  private CANSparkMax[] driveSparks = new CANSparkMax[4];
+  private CANSparkMax[] steerSparks = new CANSparkMax[4];
   private CANCoder[] canCoders;
- 
 
   private IMU IMU;
 
-  public Drive(IMU IMU, CANSparkMax[] driveMotors, CANSparkMax[] steerMotors, CANCoder[] canCoders) {
+  public Drive(IMU IMU) {
     this.IMU = IMU;
-    this.driveMotors = driveMotors;
-    this.steerMotors = steerMotors;
-    this.canCoders = canCoders;
+
+    for (int i = 0; i < 4; i++) {
+      driveSparks[i] = new CANSparkMax(Constants.DRIVE_CAN_IDS[i], MotorType.kBrushless);
+      steerSparks[i] = new CANSparkMax(Constants.STEER_CAN_IDS[i], MotorType.kBrushless);
+      canCoders[i] = new CANCoder(Constants.CANCODER_CAN_IDS[i]);
+    }
+
   }
 
   @Override
   public void periodic() {
-    
 
     // This method will be called once per scheduler run
   }
-
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
-
 
   public void runSpark(double speed) {
     //spark.set(speed);
@@ -67,6 +67,6 @@ public class Drive extends SubsystemBase {
   }
 
   public void resetEncoders() {
-    
+
   }
 }
