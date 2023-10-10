f9a0ccf2837f6f531ee6b405699c4abba74d9412
Logan MacAskill <logan@macaskill.com>
2023-10-09 23:16:40 -0700

    fix shuffleboard pid tuning

--- src/main/java/frc/robot/utils/SparkMaxPIDFTuner.java
+++ src/main/java/frc/robot/utils/SparkMaxPIDFTuner.java
@@ -20,11 +20,13 @@ public class SparkMaxPIDFTuner extends SubsystemBase {
   private SparkMaxPIDController[] followers;
   private ShuffleboardTab tab;
 
-  public SparkMaxPIDFTuner(String name, SparkMaxPIDController PIDF, Supplier<Double> setpointSupplier, Supplier<Double> measurementSupplier) {
+  public SparkMaxPIDFTuner(String name, SparkMaxPIDController PIDF, Supplier<Double> setpointSupplier,
+      Supplier<Double> measurementSupplier) {
     this(name, PIDF, new SparkMaxPIDController[] {}, setpointSupplier, measurementSupplier);
   }
 
-  public SparkMaxPIDFTuner(String name, SparkMaxPIDController PIDF, SparkMaxPIDController[] followers, Supplier<Double> setpointSupplier, Supplier<Double> measurementSupplier) {
+  public SparkMaxPIDFTuner(String name, SparkMaxPIDController PIDF, SparkMaxPIDController[] followers,
+      Supplier<Double> setpointSupplier, Supplier<Double> measurementSupplier) {
     this.PIDF = PIDF;
     this.followers = followers;
     this.setpointSupplier = setpointSupplier;
@@ -35,26 +37,26 @@ public class SparkMaxPIDFTuner extends SubsystemBase {
     kFF = PIDF.getFF();
     tab = Shuffleboard.getTab(name + " PID Tuner");
     kP_entry = tab.add(name + " kP", kP)
-      .withPosition(0, 0)
-      .getEntry();
+        .withPosition(0, 0)
+        .getEntry();
     kI_entry = tab.add(name + " kI", kI)
-      .withPosition(1, 0)
-      .getEntry();
+        .withPosition(1, 0)
+        .getEntry();
     kD_entry = tab.add(name + " kD", kD)
-      .withPosition(2, 0)
-      .getEntry();
+        .withPosition(2, 0)
+        .getEntry();
     kFF_entry = tab.add(name + " kFF", kFF)
-      .withPosition(3, 0)
-      .getEntry();
-    
-    setpoint_entry = tab.add(name + " setpoint", setpointSupplier).withWidget(BuiltInWidgets.kGraph).getEntry();
-    measurement_entry = tab.add(name + " measurement", measurementSupplier).withWidget(BuiltInWidgets.kGraph).getEntry();
-    
-    graph_entry = tab.add(name + " graph", new GenericEntry[] {setpoint_entry, measurement_entry})
-      .withPosition(0, 1)
-      .withWidget(BuiltInWidgets.kGraph)
-      .withSize(3,1)
-      .getEntry();
+        .withPosition(3, 0)
+        .getEntry();
+
+    setpoint_entry = tab.add(name + " setpoint", setpointSupplier.get()).getEntry();
+    measurement_entry = tab.add(name + " measurement", measurementSupplier.get()).getEntry();
+    graph_entry = tab.add(name + " graph", new Double[] { setpointSupplier.get(), measurementSupplier.get() })
+        .withWidget(BuiltInWidgets.kGraph)
+        .withPosition(0, 3)
+        .withWidget(BuiltInWidgets.kGraph)
+        .withSize(3, 2)
+        .getEntry();
   }
 
   @Override
@@ -64,25 +66,33 @@ public class SparkMaxPIDFTuner extends SubsystemBase {
     double new_kD = kD_entry.getDouble(0);
     double new_kFF = kFF_entry.getDouble(0);
 
+    setpoint_entry.setDouble(setpointSupplier.get());
+    measurement_entry.setDouble(measurementSupplier.get());
+    graph_entry.setDoubleArray(new Double[] { setpointSupplier.get(), measurementSupplier.get() });
+
     if (new_kP != kP) {
       kP = new_kP;
       Logger.REV(PIDF.setP(kP), "PIDF.setP");
-      for (SparkMaxPIDController follower : followers) Logger.REV(follower.setP(kP), "follower.setP");
+      for (SparkMaxPIDController follower : followers)
+        Logger.REV(follower.setP(kP), "follower.setP");
     }
     if (new_kI != kI) {
       kI = new_kI;
       Logger.REV(PIDF.setI(kI), "PIDF.setI");
-      for (SparkMaxPIDController follower : followers) Logger.REV(follower.setI(kP), "follower.setI");
+      for (SparkMaxPIDController follower : followers)
+        Logger.REV(follower.setI(kP), "follower.setI");
     }
     if (new_kD != kD) {
       kP = new_kD;
       Logger.REV(PIDF.setD(kD), "PIDF.setD");
-      for (SparkMaxPIDController follower : followers) Logger.REV(follower.setD(kP), "follower.setD");
+      for (SparkMaxPIDController follower : followers)
+        Logger.REV(follower.setD(kP), "follower.setD");
     }
     if (new_kFF != kFF) {
       kFF = new_kFF;
       Logger.REV(PIDF.setFF(kFF), "PIDF.setFF");
-      for (SparkMaxPIDController follower : followers) Logger.REV(follower.setFF(kP), "follower.setFF");
+      for (SparkMaxPIDController follower : followers)
+        Logger.REV(follower.setFF(kP), "follower.setFF");
     }
   }
 }
