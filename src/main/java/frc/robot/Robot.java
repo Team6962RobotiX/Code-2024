1f8034145a82aa3106d2c4eb31a7f2db23a097b7
RobotXTeam6962 <github@team6962.com>
2024-01-16 19:56:46 -0800

    finished kitbot code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,11 +7,13 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.io.File;
 import java.io.IOException;
 import edu.wpi.first.wpilibj.TimedRobot;
 import com.revrobotics.CANSparkMax;
+import com.fasterxml.jackson.databind.util.RawValue;
 import com.revrobotics.*;
 
 
@@ -23,13 +25,26 @@ import org.opencv.core.Mat;
 
 import java.util.*;
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  Joystick joystick0;
+  PWMSparkMax lbank;
+  PWMSparkMax rbank;
+  DifferentialDrive myDrive;
+  
   @Override
-  public void robotInit() {}
+  public void robotInit() {
 
+    //Joystick
+    joystick0 = new Joystick(0);
+
+    //Sparks
+    lbank = new PWMSparkMax(0); 
+    rbank = new PWMSparkMax(1); 
+
+    //TankDrive thingy dink
+    myDrive = new DifferentialDrive(lbank,rbank);
+  
+  }
+    
   @Override
   public void robotPeriodic() {}
 
@@ -45,7 +60,20 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
 
+    lbank.setVoltage(12);
+    rbank.setVoltage(12);
+
+    double limitTurnSpeed = 0.75; 
+
+    double joystickLValue = 
+        (-joystick0.getRawAxis(1) + (joystick0.getRawAxis(2) * limitTurnSpeed));
+    double joystickRValue =
+        (-joystick0.getRawAxis(1) - (joystick0.getRawAxis(2) * limitTurnSpeed));
+
+    myDrive.tankDrive(-joystickLValue, -joystickRValue,false);
+  }
 
 }
+
