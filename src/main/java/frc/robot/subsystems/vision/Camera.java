3f0bec1492099edb31be70b593948a47c5b4d2d4
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2024-01-30 19:05:18 -0800

    Revert "Add autoorient"
    This reverts commit d18ba1888910dcd2848144f292d60e209ffc8111.

--- src/main/java/frc/robot/subsystems/vision/Camera.java
+++ src/main/java/frc/robot/subsystems/vision/Camera.java
@@ -26,10 +26,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.PhotonUtils;
 import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.PhotonCameraSim;
@@ -43,8 +40,6 @@ import frc.robot.util.LimelightHelpers;
 import frc.robot.util.Logging.Logger;
 import frc.robot.Constants;
 import frc.robot.Constants.PHOTON_LIB;
-import java.util.List;
-import java.util.Optional;
 
 
 public class Camera extends SubsystemBase {
@@ -62,7 +57,6 @@ public class Camera extends SubsystemBase {
   private PhotonCameraSim cameraSim;
   private Supplier<Pose2d> poseSupplier;
   private NetworkTableInstance inst;
-  PhotonPoseEstimator photonPoseEstimator;
 
 
   // LimelightHelper Fiducial methods are not static so you need to make an instance of it
@@ -84,8 +78,6 @@ public class Camera extends SubsystemBase {
     catch(Exception ex) {
       DriverStation.reportError(ex.getMessage(), true);
     }
-
-    botPose = Constants.SWERVE_DRIVE.STARTING_POSE_3D;
   }
   // real camera
   public Camera(String name) {
@@ -102,8 +94,6 @@ public class Camera extends SubsystemBase {
     Translation3d robotToCameraTrl = new Translation3d(0, 0, Constants.PHOTON_LIB.CAM_HEIGHT_OFF_GROUND);
     Rotation3d robotToCameraRot = new Rotation3d(0, Math.toRadians(-Constants.PHOTON_LIB.CAM_PITCH), 0);
     Transform3d robotToCamera = new Transform3d(robotToCameraTrl, robotToCameraRot);
-    photonPoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, robotToCamera);
-
 
     visionSim.addCamera(cameraSim, robotToCamera);
     cameraSim.enableRawStream(true);
@@ -114,7 +104,6 @@ public class Camera extends SubsystemBase {
 
   @Override
   public void periodic() {
-<<<<<<< Updated upstream
     // helpME = LimelightHelpers.getCameraPose3d_TargetSpace(name);
     // System.out.println("botpose: " + helpME);  
     inst = NetworkTableInstance.getDefault();
@@ -126,87 +115,6 @@ public class Camera extends SubsystemBase {
     latestResult = camera.getLatestResult();
   }
 
-=======
-
-    
-
-
-    setEstimatedGlobalPose();
-
-
-
-    //Forward Camera
-
-    // Construct PhotonPoseEstimator
-
-    //limelightData = LimelightHelpers.getLatestResults(name);
-    //botPose = LimelightHelpers.getBotPose3d(name);
-    // helpME = LimelightHelpers.getCameraPose3d_TargetSpace(name);
-    // System.out.println("botpose: " + helpME);  
-    // //fieldSpace = LimelightHelperFidcuial.getRobotPose_FieldSpace();
-  
-    // //Pose3d targetSpace = LimelightHelpers.getCameraPose3d_TargetSpace("light"); // change later
-    // //Pose3d targetSpaceBottom = LimelightHelpers.getCameraPose3d_TargetSpace("bottom");
-    // inst = NetworkTableInstance.getDefault();
-    // NetworkTable inet = inst.getTable("limelight");
-    // NetworkTableEntry tx = inet.getEntry("tx");
-    // double x = tx.getDouble(0.0);
-    // System.out.println("table: " + x);
-
-    // //double z = Math.abs(targetSpace.getZ());
-    // //double z_bottom = Math.abs(targetSpaceBottom.getZ());
-    // latestResult = camera.getLatestResult();
-
-    // if (z_top != 0 && z_bottom != 0) {
-    //   lastKnownAprilTagZ = (z_top+z_bottom)/2;
-    // }else if (z_top != 0){
-    //   lastKnownAprilTagZ = z_top;
-    // }else if (z_bottom != 0){
-    //   lastKnownAprilTagZ = z_bottom;
-    // }
-
-    //lastKnownAprilTagZ = z;
-  }
-
-  /*public double getLastKnownAprilTagZ() {
-    return lastKnownAprilTagZ;
-  }
-  */
-
-  public int getFiducialId(){
-    var result = camera.getLatestResult();
-    boolean hasTargets = result.hasTargets();
-    
-    List<PhotonTrackedTarget> targets = result.getTargets();
-
-    PhotonTrackedTarget target = result.getBestTarget();
-
-    if (hasTargets) {
-      return target.getFiducialId();
-    }
-    else {
-      return 0;
-    }
-  }
-
-  public void setEstimatedGlobalPose() {
-    photonPoseEstimator.setReferencePose(botPose);
-    Optional<EstimatedRobotPose> raw = photonPoseEstimator.update();
-    if (raw.isPresent()){
-      EstimatedRobotPose poseList = raw.get();
-      Pose3d pose = poseList.estimatedPose;
-      botPose = pose;
-      //System.out.println(pose);
-
-    }
-  }
-
-  public Pose3d getPose(){
-    return botPose;
-  }
-
-
->>>>>>> Stashed changes
   public LimelightHelpers.Results getTargetingResults() {
     return limelightData.targetingResults;
   }
