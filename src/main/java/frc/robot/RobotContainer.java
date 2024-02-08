b18df5016587bbc5b3de556ccae3ee0b94038806
RobotXTeam6962 <github@team6962.com>
2024-02-07 19:32:39 -0800

    fixed vision pose estimation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.ResourceBundle.Control;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DataLogManager;
