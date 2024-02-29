ef3ce54d183749836c32e816e47099cc26ac827b
RobotXTeam6962 <github@team6962.com>
2024-02-29 10:08:37 -0800

    fix status checks

--- src/main/java/frc/robot/util/software/Logging/StatusChecks.java
+++ src/main/java/frc/robot/util/software/Logging/StatusChecks.java
@@ -9,14 +9,15 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StatusChecks {
   public static int row = 0;
   public static int column = 1;
-  private static ShuffleboardTab tab = Shuffleboard.getTab("Logging");
-  private static ComplexWidget refreshButton = tab.add("Refresh", Commands.runOnce(() -> refresh())).withWidget(BuiltInWidgets.kCommand).withSize(1, 1).withPosition(0, 0);
+  private static ShuffleboardTab tab = Shuffleboard.getTab("Status Checks");
+  private static ComplexWidget refreshButton = tab.add("Refresh", StatusChecks.refresh()).withWidget(BuiltInWidgets.kCommand).withSize(1, 1).withPosition(0, 0);
   private static Map<String, BooleanSupplier> suppliers = new HashMap<String, BooleanSupplier>();
   private static Map<String, GenericEntry> entries = new HashMap<String, GenericEntry>();
 
@@ -34,9 +35,11 @@ public class StatusChecks {
     addCheck(subsystem.getClass().getSimpleName() + "/" + name, supplier);
   }
 
-  public static void refresh() {
-    for (Map.Entry<String, BooleanSupplier> supplier : suppliers.entrySet()) {
-      entries.get(supplier.getKey()).setBoolean(supplier.getValue().getAsBoolean());
-    }
+  public static Command refresh() {
+    return Commands.runOnce(() -> {
+      for (Map.Entry<String, BooleanSupplier> supplier : suppliers.entrySet()) {
+        entries.get(supplier.getKey()).setBoolean(supplier.getValue().getAsBoolean());
+      }
+    }).ignoringDisable(true);
   }
 }
