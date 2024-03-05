af002521ba7c6c093683319a14da936b610cc163
Logan MacAskill <logan@macaskill.com>
2024-03-04 23:59:48 -0800

    Update AprilTags.java

--- src/main/java/frc/robot/subsystems/vision/AprilTags.java
+++ src/main/java/frc/robot/subsystems/vision/AprilTags.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.Constants.SWERVE_DRIVE;
 import frc.robot.Constants.Field;
 import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.drive.SwerveDrive;
@@ -19,20 +18,15 @@ import frc.robot.util.software.LimelightHelpers.PoseEstimate;
 
 public class AprilTags extends SubsystemBase {
   public static void injectVisionData(Map<String, Pose3d> cameraPoses, SwerveDrive swerveDrive) {
-    if (swerveDrive.getRotationalVelocity() > SWERVE_DRIVE.PHYSICS.MAX_ANGULAR_VELOCITY / 10.0) {
-      return;
-    }
-
     List<LimelightHelpers.PoseEstimate> poseEstimates = cameraPoses.keySet().stream().map(LimelightHelpers::getBotPoseEstimate_wpiBlue).collect(Collectors.toList());
 
     int tagCount = 0;
     for (PoseEstimate poseEstimate : poseEstimates) {
       tagCount += poseEstimate.tagCount;
     }
-    if (tagCount == 0) return;
+    if (tagCount <= 1) return;
 
     for (PoseEstimate poseEstimate : poseEstimates) {
-      if (tagCount == 1) continue;
       if (poseEstimate.pose.getTranslation().getDistance(swerveDrive.getPose().getTranslation()) > (Field.LENGTH + Field.WIDTH) * 2.0) continue;
       if (poseEstimate.pose.getX() < 0.0 || poseEstimate.pose.getY() < 0.0 || poseEstimate.pose.getX() > Field.LENGTH || poseEstimate.pose.getY() > Field.WIDTH) continue;
       
